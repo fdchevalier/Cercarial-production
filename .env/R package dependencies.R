31696b1280983c80bf736d6c74dbbb863186534f
fdchevalier <fcheval@txbiomed.org>
2020-06-19 18:33:50 -0500

    R packages installation script added

---
+++ .env/R package dependencies.R
@@ -0,0 +1,9 @@
+# System
+options("repos" = c(CRAN = "https://cran.revolutionanalytics.com"))
+library("devtools")
+library("BiocManager")
+
+install_github("jalvesaq/colorout", ref="7ea9440", upgrade="never")
+
+
+#install_bioc("3.8/rtracklayer", upgrade="never")
