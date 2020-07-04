c323aac694363e3aa0fd708179ed584e570c6318
fdchevalier <fcheval@txbiomed.org>
2020-07-03 21:47:02 -0500

    Candidate gene script and functions up and running
    Site filtration
    Score computation added.

--- scripts/Candidate_genes.R
+++ scripts/Candidate_genes.R
@@ -21,6 +21,8 @@
 
 # v0.0 - 2016-02-18: creation
 
+
+
 #===================#
 # Packages required #
 #===================#
@@ -29,10 +31,10 @@ library("qtl")      # For R/qtl commands
 library("vcfR")
 library("magrittr")
 
+library("parallel")
+library("doParallel")
 
-##-------------------
-## Packages
-##-------------------
+library("rtracklayer")
 
 #library(latticeExtra)
 
@@ -55,7 +57,14 @@ graph_fd   <- "../graphs/"
 result_fd1 <- "../results/1-QTL/"
 result_fd2 <- "../results/2-Candidate genes/"
 
-myvcf_f <- paste0(data_fd, "calling/cerc_prod_snpEff_1-3-5.vcf.gz")
+myvcf_f  <- paste0(data_fd, "calling/cerc_prod_snpEff_1-3-5.vcf.gz")
+mygff_f  <- paste0(data_fd, "genome/schistosoma_mansoni.PRJEA36577.WBPS14.annotations.gff3")
+myann_f  <- "~/data/sm_Gene_table/Sm_v7.1_ann/Sm_v7.1_transcript_table_gff-hhpred.tsv" 
+# Expression data
+myexpr_f <- "~/data/sm_Gene_table/TPM_isoforms_Sm_v7.1.tsv"
+myexpr.cln <- 3:4    # Column containing juvenil and adult expression data
+myexpr.nm  <- paste0("TPM ", c("sp 48h", "cercariae"))
+
 
 myqtl.ls <- paste0(result_fd1, "myqtl.ls.RData")
 
@@ -124,16 +133,25 @@ mylod.trsh <- 0.05
 if (mymissing.data < 0 | mymissing.data >= 1) { stop("mymissing.data must be in [0;1[") }
 if (! any(grepl(pheno.cln, myprefixes[,1]))) {stop("The pheno.cln value is unknown in myprefixes table.", call.=FALSE)}
 
-if (! dir.exists(result_fd)) { dir.create(result_fd, recursive = TRUE)
+if (! dir.exists(result_fd2)) { dir.create(result_fd2, recursive = TRUE) }
 
 #--------------#
 # Data Loading #
 #--------------#
 
+mygff <- readGFF(mygff_f)
+myann <- read.csv(myann_f, header=TRUE, sep="\t", stringsAsFactors = FALSE)
+
+# Expression data
+myexpr <- read.csv(myexpr_f, header=TRUE, sep="\t", stringsAsFactors = FALSE)
+
 # Load QTL data
 load(myqtl.ls)
 
-mypeaks    <- summary(myqtl.ls[[3]][["mr"]]$lod, perm=myqtl.ls[[3]][["mr"]]$perm, alpha=mylod.trsh)
+mylod     <- myqtl.ls[["combination"]][["em"]]$lod
+myperm    <- myqtl.ls[["combination"]][["em"]]$perm
+
+mypeaks   <- summary(mylod, perm=myperm, alpha=mylod.trsh)
 
 myqtl.chr <- mypeaks[rownames(mypeaks[grep("_[0-9ZW]$", mypeaks[,1], perl=TRUE), ]), 1]
 
@@ -148,72 +166,114 @@ myvcf.bk <- myvcf
 myvcf@gt <- myvcf@gt[, colnames(myvcf@gt) %in% c("FORMAT", mypA, mypB)]
 
 
+# GT, GQ and DP data file
+mydata <- vector("list", length(myfmt[,1])+1)
+names(mydata) <- c("FIX", myfmt[,1])
+
+mydata[["FIX"]] <- getFIX(myvcf, getINFO = TRUE)
+
+for (i in myfmt[,1]) {
+    mynum <- myfmt[ myfmt[,1] %in% i, 2]
+    mydata[[i]] <- extract.gt(myvcf, i, as.numeric = mynum)
+}
+
+# GQ
+mat.gq <- mydata$GQ < gq.trsh
+
+# DP
+mat.dp <- mydata$DP < dp.trsh
+
+mat.sel <- apply(mat.gq * mat.dp, 2, as.logical)
+
+mydata$GQ[mat.sel] <- NA
+mydata$GT[mat.sel] <- NA
+mydata$DP[mat.sel] <- NA
+
+
 # Select markers within QTLs
-myvec <- sapply(lodint.ls, function(x) getFIX(myvcf)[, 1] == x[1,1] & (getFIX(myvcf)[,2] >= x[1,2] & getFIX(myvcf)[,2] <= x[3,2])) %>% rowSums(.) > 0
-myvcf <- myvcf[myvec, ]
+myvec.mrkr <- sapply(lodint.ls, function(x) getFIX(myvcf)[, 1] == x[1,1] & as.numeric(getFIX(myvcf)[,2]) >= as.numeric(x[1,2]) & as.numeric(getFIX(myvcf)[,2]) <= as.numeric(x[3,2])) %>% rowSums(.) > 0
 
 # Select site that are all NAs
-myvec.na <- extract.gt(myvcf, "GQ") %>% apply(., 1, function(x) all(is.na(x)))
-
-# Select sites that are all below DP threshold
-myvec.dp <- (extract.gt(myvcf, "DP") < dp.trsh) %>% apply(., 1, function(x) all(x, na.rm=TRUE))
-
-# Select sites that are all below GQ threshold
-myvec.gq <- (extract.gt(myvcf, "GQ") < gq.trsh) %>% apply(., 1, function(x) all(x, na.rm=TRUE))
-
-# Select uninformative markers (same as reference or between all parents)
-myvec.mrkr <- extract.gt(a) %>% gsub("|", "/", ., fixed = TRUE) %>% apply(., 1, function(x) all(x %in% "1/1", na.rm=TRUE) | all(x %in% "0/0", na.rm=TRUE))
+myvec.na <- mydata$GT %>% apply(., 1, function(x) all(is.na(x)))
 
 # Remove sites that are below thresholds
-myvec <- rowSums(cbind(myvec.na, myvec.dp, myvec.gq, myvec.mrkr)) > 0
-myvcf <- myvcf[! myvec, ]
+# myvec <- rowSums(cbind(! myvec.mrkr, myvec.na, myvec.dp, myvec.gq, myvec.uninfo)) > 0
+myvec <- rowSums(cbind(! myvec.mrkr, myvec.na)) > 0
 
+# Clean the lists
+mydata <- lapply(mydata, function(x) x[! myvec,])
 
+# Select uninformative markers (same as reference or between all parents)
+myvec.uninfo <- mydata$GT %>% gsub("|", "/", ., fixed = TRUE) %>% apply(., 1, function(x) strsplit(x, "[/]") %>% unlist) %>% lapply(., function(x) unique(x) %>% length(.) == 1) %>% unlist()
 
+# Remove sites that are below thresholds
+# myvcf <- myvcf[! myvec.uninfo, ]
+mydata <- lapply(mydata, function(x) x[! myvec.uninfo,])
 
-#---------------------
-# Loading data table
-#---------------------
+# Find LOD score when available
+mylod.cln <- apply(mydata$FIX[,1:2], 1, function(x) mylod[mylod[,1] == x[1] & mylod[,2] == x[2], 3]) %>% lapply(., function(x) if (length(x) == 0 ) { NA } else { x }) %>% unlist()
 
-mydata <- read.table("tables/combination.em.tsv", dec=".", sep="\t", header=T)
+# Genotype score
+mysc.cln <- matrix(NA, ncol=2, nrow=nrow(mydata$GT))
+colnames(mysc.cln) <- c("A", "B")
 
-#------------------------------
-# subseting the initial table
-#------------------------------
+myp <- list(A=mypA, B=mypB)
 
-# Order LOD table
-mydata.ordered <- mydata[order(mydata[,3], decreasing=T),]
+for(i in 1:length(myp)) {
+    mydata.tmp <- mydata$GT[, myp[[i]]]
+    mysc.cln[, i] <- apply(mydata.tmp, 1, function(x) {
+                   
+                                       myna  <- is.na(x)
+                                       myhet <- is_het(as.matrix(x), na_is_false = FALSE)
 
-# QTL on x fist chr
-x <- 4
-mychroms <- unique(mydata.ordered[,1])[1:x]
+                                       if (all(myna)) { # If all is NA
+                                           y <- 0
+                                       } else if (any(myna)) { # If one NA
+                                           if (all(na.omit(myhet))) { # If remaining genotype heterozygous
+                                               y <- 0
+                                           } else if (any(grepl("0[/|]0", x))) { # If homozygous by deduction is all homozygous for the reference
+                                               y <- 0
+                                           } else { # If homozygous for the alternative
+                                               y <- 5
+                                           }
+                                       } else if (all(! myna)) { # If no NA
+                                           if (all(myhet)) { # If all heterozygous
+                                               y <- 0
+                                           } else if (any(myhet)) { # If one homozygous
+                                               if (any(grepl("0[/|]0", x))) { # If homozygous site is the same as reference
+                                                   y <- 0
+                                               } else {
+                                                   y <- 10
+                                               }
+                                           } else if (all(! myhet)) { # If no heterozygous
+                                               if (strsplit(x, "[/|]") %>% unlist %>% unique %>% length() == 1) { # Same allele everywhere
+                                                   y <- 0
+                                               } else {
+                                                   y <- 20
+                                               }
+                                           }
+                                       }
 
-for (c in mychroms) {
-	# New data tables corresponding to each chromosome of interest
-	mychrom <- mydata[ mydata[,1] == c, ]
-
-	# Finding the maximum LOD value for each chromosome of interest
-	mychrom.max <- max(mychrom$lod)
-
-	# Calculate the max lod value - 1.8 lod
-	mychrom.1.8 <- mychrom.max - 1.8
-#	mychrom.1.8 <- mychrom.max
-
-	# Searching all the row of the chromosome table that have a lod value >= to the max lod -1.8 lod
-	mychrom.1.8 <- mychrom[ mychrom[,3] >= mychrom.1.8 , ]
-	mychrom.1.8 <- mychrom.1.8[c(1,nrow(mychrom.1.8)),]
-	print(mychrom.1.8)
-	
-	# Subtract position to get segment length
-	myseg <- diff(as.numeric(as.vector(mychrom.1.8[,2])))
-	
-	# Average number of gene by bp
-	mygenes <- 10000 / 364500000
-	
-	# Number genes expected in segment
-	mygenes.nb <- myseg * mygenes
-	
-	# Print final message
-	print(paste0("Number of expected genes under QTL of ", c, ": ", mygenes.nb))
+                                       return(y)
+                                })
 
 }
+
+
+
+# Build table
+mydata.tb <- data.frame(mydata$FIX, mydata$GT[, c(mypA, mypB)], mysc.cln, lod=mylod.cln)
+
+mygff.genes <- mygff[ mygff[,3] == "gene", ]
+
+tb.ls <- vector("list", length(myqtl.chr))
+names(tb.ls) <- myqtl.chr
+
+for (i in myqtl.chr) {
+    tb.ls[[i]]$qtl.tb <- qtl.tb(mydata.tb, chr = i, myann, myexpr, myexpr.cln, expr.nm=myexpr.nm, cln.print=9:15)
+    tb.ls[[i]]$qtl.tb.sum <- qtl.tb.sum(tb.ls[[i]]$qtl.tb, myann, myexpr, myexpr.cln, myexpr.nm, mygff.genes,baits.bed=NULL)
+
+    write.table(tb.ls[[i]]$qtl.tb, paste0(result_fd2, "/", i, "_QTL_table_details.tsv"), row.names = FALSE, quote = FALSE, sep = "\t")
+    write.table(tb.ls[[i]]$qtl.tb.sum, paste0(result_fd2, "/", i, "_QTL_table_summary.tsv"), row.names = FALSE, quote = FALSE, sep = "\t")
+}
+
