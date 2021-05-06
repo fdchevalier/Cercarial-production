788553433f9cdb7957e593ee08e28641eb34bf28
fdchevalier <fcheval@txbiomed.org>
2021-05-06 14:40:24 -0500

    Add header to rename_chr.R file

--- scripts/functions/rename_chr.R
+++ scripts/functions/rename_chr.R
@@ -1,3 +1,32 @@
+#!/usr/bin/env Rscript
+# Title: rename_chr.R
+# Version: 0.0
+# Author: Frédéric CHEVALIER <fcheval@txbiomed.org>
+# Created in: 2020-06-30
+# Modified in:
+
+
+
+#==========#
+# Comments #
+#==========#
+
+# Rename chromosome ID of the V7 S. mansoni genome to the form of the V5 for script backward compatibility.
+
+
+
+#==========#
+# Versions #
+#==========#
+
+# v0.0 - 2020-06-30: creation
+
+
+
+#==========#
+# Function #
+#==========#
+
 rename_chr_SmV7 <- function(x, cln) {
     y <- as.character(x[,cln])
 
